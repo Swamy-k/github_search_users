61a6d8c48983e06808e1033c5b06577a4e0895e4
Kumaraswamy <kosurukumaraswamy007@gmail.com>
2020-02-21 08:27:12 +0530

    first_commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import Form from "./components/Github/Form";
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+          <Form />
+       
+      </div>
+    );
+  }
+}
+
+export default App;
